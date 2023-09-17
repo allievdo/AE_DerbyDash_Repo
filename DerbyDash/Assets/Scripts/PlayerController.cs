caba02a722e1e0efb49697c60c553fb4e1659912
allikitten <allison.evdokimo@cox.net>
2023-09-17 12:13:36 -0700

    :)

--- DerbyDash/Assets/Scripts/PlayerController.cs
+++ DerbyDash/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,10 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
 
-    public float runningForce;
+    public float runningForce = 3f;
+
     void FixedUpdate()
-    {
+    { 
         rb.velocity = new Vector2(runningForce, 0f);
     }
-
 }
